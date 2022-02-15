9cc31bc86c4064baaed553c343b7e50270fab291
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-15 19:03:53 +0000

    JoshOps-001 fix typo in count.index

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -7,12 +7,12 @@ resource "aws_vpc" "application_vpc" {
 }
 
 resource "aws_subnet" "main" {
-    count            = length(data.aws_avaiability_zones.available.names)
+    count               = length(data.aws_avaiability_zones.available.names)
     vpc_id              = aws_vpc.application_vpc.id
-    cidr_block        = cidrsubnet(aws_vpc.application_vpc.cidr_block, 3, count.index)
+    cidr_block          = cidrsubnet(aws_vpc.application_vpc.cidr_block, 3, count.index)
     availability_zone   = data.aws_availability_zones.available.names[count.index]
 
     tags = {
-        Name = "public_subnet_${cound.index}"
+        Name = "public_subnet_${count.index}"
     }
 }
