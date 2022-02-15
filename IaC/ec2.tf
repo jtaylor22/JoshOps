92e76a49aa94573d1500e1667f273e1bbcbf285f
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-15 19:55:12 +0000

    JoshOps-001 fix route table association

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -28,7 +28,7 @@ resource "aws_route_table" "igw_route_table" {
 }
 
 resource "aws_route_table_association" "igw_route_table_association" {
-  subnet_id      = aws_subnet.public_subnet.[count.index].id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.igw_route_table.id
 }
 
