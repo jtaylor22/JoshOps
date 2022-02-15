6f35c62fc01a5676d96b0fcf08daf4080547554d
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-15 19:42:51 +0000

    JoshOps-001 create internet gateway, route table and private subnets

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -1,12 +1,38 @@
 resource "aws_vpc" "application_vpc" {
-  cidr_block       = "172.31.0.0/24"
+  cidr_block       = var.vpc_cidr_block
 
   tags = {
     Name = "Application_VPC"
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.application_vpc.id
+
+  tags = {
+    Name = "Application_IGW"
+  }
+}
+
+resource "aws_route_table" "igw_route_table" {
+  vpc_id = aws_vpc.application_vpc.id
+
+  route {
+    cidr_block = var.vpc_cidr_block
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = "IGW_route_table"
+  }
+}
+
+resource "aws_route_table_association" "igw_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.igw_route_table.id
+}
+
+resource "aws_subnet" "public_subnet" {
     count               = length(data.aws_availability_zones.available.names)
     vpc_id              = aws_vpc.application_vpc.id
     cidr_block          = cidrsubnet(aws_vpc.application_vpc.cidr_block, 3, count.index)
@@ -14,5 +40,16 @@ resource "aws_subnet" "main" {
 
     tags = {
         Name = "public_subnet_${count.index}"
+    }
+}
+
+resource "aws_subnet" "private_subnet" {
+    count               = length(data.aws_availability_zones.available.names)
+    vpc_id              = aws_vpc.application_vpc.id
+    cidr_block          = cidrsubnet(aws_vpc.application_vpc.cidr_block, 3, count.index)
+    availability_zone   = data.aws_availability_zones.available.names[count.index]
+
+    tags = {
+        Name = "private_subnet_${count.index}"
     }
 }
