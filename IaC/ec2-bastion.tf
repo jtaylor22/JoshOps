49241c9ab79d74283d9ea79246a949ee1319caf8
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-16 15:14:10 +0000

    JoshOps-002: create jenkins instance and ALB

--- IaC/ec2-bastion.tf
+++ IaC/ec2-bastion.tf
@@ -12,33 +12,33 @@
 #   }
 # }
 
-# resource "aws_network_interface" "bastion_interface" {
-#   subnet_id       = aws_subnet.public_subnet[0].id
-#   security_groups = [aws_security_group.allow_instance_connect.id]
+resource "aws_network_interface" "bastion_interface" {
+  subnet_id       = aws_subnet.public_subnet[0].id
+  security_groups = [aws_security_group.allow_instance_connect.id]
 
-#   tags = {
-#     Name = "bastion_network_interface"
-#   }
-# }
+  tags = {
+    Name = "bastion_network_interface"
+  }
+}
 
-# resource "aws_eip" "elastic_ip" {
-#   depends_on = [aws_internet_gateway.internet_gateway]
-#   count      = length(data.aws_availability_zones.available.names)
-#   vpc        = true
+resource "aws_eip" "elastic_ip" {
+  depends_on = [aws_internet_gateway.internet_gateway]
+  count      = length(data.aws_availability_zones.available.names)
+  vpc        = true
 
-#   tags = {
-#     Name = "EIP_${count.index}"
-#   }
-# }
+  tags = {
+    Name = "EIP_${count.index}"
+  }
+}
 
-# resource "aws_eip" "bastion_eip" {
-#   depends_on = [aws_internet_gateway.internet_gateway]
-#   vpc        = true
+resource "aws_eip" "bastion_eip" {
+  depends_on = [aws_internet_gateway.internet_gateway]
+  vpc        = true
 
-#   tags = {
-#     Name = "bastion_eip"
-#   }
-# }
+  tags = {
+    Name = "bastion_eip"
+  }
+}
 
 # resource "aws_eip_association" "bastion_eip_assoc" {
 #   instance_id          = aws_instance.bastion_instance.id
@@ -46,28 +46,28 @@
 #   network_interface_id = aws_network_interface.bastion_interface.id
 # }
 
-# resource "aws_security_group" "allow_instance_connect" {
-#   name        = "allow_instance_connect"
-#   description = "Allow connectivity to AWS Instance Connect"
-#   vpc_id      = aws_vpc.application_vpc.id
+resource "aws_security_group" "allow_instance_connect" {
+  name        = "allow_instance_connect"
+  description = "Allow connectivity to AWS Instance Connect"
+  vpc_id      = aws_vpc.application_vpc.id
 
-#   ingress {
-#     description = "SSH from EC2_INSTANCE_CONNECT service in eu-west-2 "
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["3.8.37.24/29"]
-#   }
+  ingress {
+    description = "SSH from EC2_INSTANCE_CONNECT service in eu-west-2 "
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.8.37.24/29"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "allow_instance_connect_sg"
-#   }
-# }
+  tags = {
+    Name = "allow_instance_connect_sg"
+  }
+}
