df21fa2da7dd6d61afdd1e1e912d6c200b309709
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-16 17:27:35 +0000

    JoshOps-002: create jenkins instance and ALB

--- IaC/user-data.sh
+++ IaC/user-data.sh
@@ -2,9 +2,10 @@
 
 sudo amazon-linux-extras install epel -y
 sudo yum update -y
-
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+
+sudo yum install jenkins -y
 
 sudo systemctl start jenkins
