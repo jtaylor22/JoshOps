f2dc4fa0d4b3e13f97fd848408f7fb4d9ae76ff8
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-16 01:24:38 +0000

    JoshOps-002: create bastion host

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -136,3 +136,29 @@ resource "aws_instance" "bastion_instance" {
     Name = "bastion_host"
   }
 }
+
+resource "aws_security_group" "allow_instance_connect" {
+  name        = "allow_instance_connect"
+  description = "Allow connectivity to AWS Instance Connect"
+  vpc_id      = aws_vpc.application_vpc.id
+
+  ingress {
+    description      = "SSH from EC2_INSTANCE_CONNECT service in eu-west-2 "
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["3.8.37.24/29"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_instance_connect_sg"
+  }
+}
