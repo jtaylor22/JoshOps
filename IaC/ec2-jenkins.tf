f53d02e483702b2ab74296b20b77c27f48941443
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-16 16:45:30 +0000

    JoshOps-002: create jenkins instance and ALB

--- IaC/ec2-jenkins.tf
+++ IaC/ec2-jenkins.tf
@@ -27,22 +27,6 @@ resource "aws_security_group" "jenkins_security_group" {
   description = "Allow HTTPS connectivity to Jenkins"
   vpc_id      = aws_vpc.application_vpc.id
 
-  ingress {
-    description = "443 Ingress"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name = "jenkins_sg"
   }
