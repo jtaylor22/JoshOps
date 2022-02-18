7a885a658d01bbfc76a33bb90d190209ba11aa15
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-18 01:22:05 +0000

    JoshOps-003: create jenkins slave

--- IaC/user_data_install_java.sh
+++ IaC/user_data_install_java.sh
@@ -7,5 +7,4 @@ sudo mkdir /var/jenkins
 
 sudo aws s3 cp s3://test-joshops-bucket/agent.jar /var/jenkins
 
-sudo java -jar /var/jenkins/agent.jar -jnlpUrl https://jenkins.joshdevops.co.uk/computer/slave%5F1/jenkins-agent.jnlp -secret 10eedc6fe3b188b7a50a43ac8c0577e1426929c8a82bfec8ce9aefba3db89b80 -workDir "/va
-r/jenkins"
+sudo java -jar /var/jenkins/agent.jar -jnlpUrl https://jenkins.joshdevops.co.uk/computer/slave%5F1/jenkins-agent.jnlp -secret 10eedc6fe3b188b7a50a43ac8c0577e1426929c8a82bfec8ce9aefba3db89b80 -workDir "/var/jenkins"
