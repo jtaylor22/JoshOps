5949b6304fe91468c2088a9b66881cf0c89d49ba
Joshua <JoshuaTaylor@Kennedies-iMac.local>
2022-02-18 21:06:27 +0000

    JoshOps-003: create splunk instance and ALB

--- IaC/ec2-splunk.tf
+++ IaC/ec2-splunk.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "splunk_instance" {
   ami           = data.aws_ami.splunk_ami.id
   instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.jenkins_key_pair.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.splunk_interface.id
